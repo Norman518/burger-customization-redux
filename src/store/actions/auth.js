0d111904a851f5c1a3f6d0878d95570f426b26a9
Vincent T <46739285+Norman518@users.noreply.github.com>
2019-12-24 08:47:42 -0500

    added sign in and sign up auth

---
+++ src/store/actions/auth.js
@@ -0,0 +1,49 @@
+import * as actionTypes from './actionTypes';
+import axios from 'axios';
+
+export const authStart = () => {
+  return {
+    type: actionTypes.AUTH_START,
+  };
+};
+
+export const authSuccess = authData => {
+  return {
+    type: actionTypes.AUTH_SUCCESS,
+    authData,
+  };
+};
+
+export const authFail = error => {
+  return {
+    type: actionTypes.AUTH_FAIL,
+    error,
+  };
+};
+
+export const auth = (email, password, isSignup) => {
+  return dispatch => {
+    dispatch(authStart());
+    const authData = {
+      email,
+      password,
+      returnSecureToken: true,
+    };
+    let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDbkq7i24xMJ7uO7RlURyfI_I51QSYYXVw'
+    if (!isSignup) {
+      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDbkq7i24xMJ7uO7RlURyfI_I51QSYYXVw'
+    }
+    axios
+      .post(
+        url,
+        authData,
+      )
+      .then(response => {
+        dispatch(authSuccess(response.data));
+      })
+      .catch(error => {
+        console.log(error);
+        dispatch(authFail(error));
+      });
+  };
+};
