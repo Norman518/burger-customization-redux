fa78d9231f39ffd9415503719a8ab322efa337e0
Vincent T <46739285+Norman518@users.noreply.github.com>
2019-12-25 12:47:51 -0500

    logged users are able to order

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -110,7 +110,7 @@ class ContactData extends Component {
       orderData: formData,
     };
 
-    this.props.onOrderBurger(order);
+    this.props.onOrderBurger(order, this.props.token);
   };
 
   checkValidity(value, rules) {
@@ -210,12 +210,13 @@ const mapStateToProps = state => {
     ingredients: state.burgerBuilder.ingredients,
     price: state.burgerBuilder.totalPrice,
     loading: state.order.loading,
+    token: state.auth.token,
   };
 };
 
 const mapDispatchToProps = dispatch => {
   return {
-    onOrderBurger: orderData => dispatch(actions.buyBurger(orderData)),
+    onOrderBurger: (orderData, token) => dispatch(actions.buyBurger(orderData, token)),
   };
 };
 
